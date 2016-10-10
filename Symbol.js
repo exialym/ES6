2e0e5e78992db9a6736a914dd6647d928f76469c
exialym <exialym@icloud.com>
2016-10-10 23:02:27 +0800

    Symble
    基本

---
+++ Symbol.js
@@ -0,0 +1,15 @@
+import 'babel-polyfill';
+
+// 没有参数的情况
+var s1 = Symbol();
+var s2 = Symbol();
+s1 === s2 // false
+s1.toString() // "Symbol()"
+s2.toString() // "Symbol()"
+
+// 有参数的情况
+var s1 = Symbol("foo");
+var s2 = Symbol("foo");
+s1 === s2 // false
+console.log(s1);//.toString() // "Symbol(foo)"
+console.log(s2);//.toString() // "Symbol(bar)"
